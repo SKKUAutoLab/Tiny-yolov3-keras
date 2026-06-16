8a2d2e1433c12f7e174c0933e9a293a76c9cf329
cockioh2 <cockioh2@naver.com>
2026-06-16 13:30:39 +0900

    Change data/convert.py more path-robustly, delete docker_run.sh (deprecated)

--- data/convert.py
+++ data/convert.py
@@ -1,7 +1,9 @@
+import os
+
 # 파일 경로
 file_path = './dataset/train/_annotations.txt'
 
-#본인의 데이터셋 경로가 맞는지 검증
+# 본인의 데이터셋 경로가 맞는지 검증 (끝에 /가 있으므로 안심하고 결합 가능)
 new_base_path = '/workspace/vitis-ai-yolov3-tiny/data/dataset/train/'
 
 # 파일 읽기
@@ -12,8 +14,12 @@ with open(file_path, 'r') as file:
 updated_lines = []
 for line in lines:
     if line.strip():  # 빈 줄이 아닌 경우에만 처리
-        parts = line.split()  # 어노테이션 파일의 형식을 기준으로 분리
-        image_name = parts[0]  # 첫 번째 요소가 이미지 이름
+        parts = line.split()  # 어노테이션 파일의 형식을 공백 기준으로 분리
+        
+        # os.path.basename을 사용해 앞의 상대 경로('../.../')를 모두 날리고 파일명만 추출
+        image_name = os.path.basename(parts[0])  
+        
+        # 깔끔해진 파일명과 새 베이스 경로 결합
         new_line = f"{new_base_path}{image_name} {' '.join(parts[1:])}\n"
         updated_lines.append(new_line)
     else:
